5d403524420cedb9f7c6286f291ee7ca87acdf31
pyada <pyada@10.0.0.56>
2022-02-22 12:38:34 -0500

    done.completed

---
+++ source/JavaPrograms/IntegerExp.java
@@ -0,0 +1,15 @@
+package JavaPrograms;
+
+import java.util.Scanner;
+
+public class IntegerExp {
+
+	public static void main(String[] args) {
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num = reader.nextInt();
+		System.out.println("you entered " +num);
+		}
+
+}
---
+++ source/JavaPrograms/UppercaseAlpha.java
@@ -0,0 +1,13 @@
+package JavaPrograms;
+
+public class UppercaseAlpha {
+
+	public static void main(String[] args) {
+		char c;
+		System.out.println("UPPER CASE");
+	    for(c = 'A'; c <= 'Z'; ++c)
+	    	
+	      System.out.print(c + " ");
+	}
+
+}
